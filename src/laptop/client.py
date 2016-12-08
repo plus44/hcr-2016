dc167f052435e48576129fc2e006978a57f8bbe0
plus44 <mr3313@ic.ac.uk>
2016-12-08 05:11:20 +0000

    Added state machine ffor asynchronous event handling, added OCR capability directly through the tesseract python bindings.

--- src/laptop/client.py
+++ src/laptop/client.py
@@ -4,9 +4,10 @@
 
 # STANDARD PYTHON IMPORTS
 import json
+import base64
+import httplib
 
 # PYTHON LIBRARIES
-import httplib
 
 # USER LIBRARIES
 
@@ -16,21 +17,45 @@ import httplib
 class LaptopClient():
 	""" Exposes a simple interface to the main program for accessing the server.
 	"""
-	def __init__(self, host, port=80):
-		''' Initialises the client on default port 80, on a provided host.
+	def __init__(self, host, port=80, state_machine=None):
+		''' Initialises the network client on the laptop.
+
+			'host' is the server address
+			'port' is the server port (80 for HTTP)
+			'state_machine' is a reference to the state_machine that contains a 
+				function 'handler_done_long_polling(image)' to handle the end of 
+				long-polls.
 		'''
 		self._host = host
 		self._port = port
+		self._state_machine = state_machine
 		self.post_req = {"doneProcessing" : False, "error" : "NotInitialized"}
 		self.conn = httplib.HTTPConnection(self._host, self._port)
+		self.is_long_polling = False
+		self.decoded_image = ""
+
+	def queue_long_poll(self):
+		''' Queues a long-poll to the server on a background thread. When the 
+		long-polling is complete, self.is_long_polling will be deasserted and
+		the result of the request will be in self.decoded_image. 
+
+		Additionally, the state_machine's handler_done_long_polling() method 
+		will be called with the decoded image as the single parameter.
+
+		TODO: BACKGROUND THREAD
+		'''
+		self.decoded_image = self.long_poll()
+		self._state_machine.handler_done_long_polling(self.decoded_image)
 
 	def long_poll(self):
 		''' Long polls the server. If code 204 is returned, the server has timed
 		out and the long-poll is re-initiated. 
 	
-		A valid return of the laptop's long-poll will contain a picture.
+		A valid return of the laptop's long-poll will contain a base64 decoded 
+			picture, as per the server's specification.
 		'''
 		try:
+			self.is_long_polling = True
 			self.conn.request("GET", "/laptop", "")
 			response = self.conn.getresponse()
 		
@@ -39,8 +64,10 @@ class LaptopClient():
 				response = self.conn.getresponse()
 
 			if response.status == 200:
-				return response.read()
+				self.is_long_polling = False
+				return response.read().decode('base64')
 		except:
+			self.is_long_polling = False
 			print "Returning from laptop long-polling."
 			return ""
 
--- src/laptop/main.py
+++ src/laptop/main.py
@@ -6,35 +6,139 @@
 import argparse
 import sys
 import time
+import base64
 
 # PYTHON LIBRARIES
 
 # USER LIBRARIES
+import enum
 from client import LaptopClient
+from ocr import OCRController
 
 # GLOBAL VARIABLES
 
 # CLASSES
+class GlobalStateMachine():
+	''' Describes the state machine that handles server, OCR, speech, gestures
+	'''
+	def __init__(self, client=None, ocr=None):
+		''' Initialise the state machine
+
+			'client' is a LaptopClient() instance
+			'ocr' is an OCRController() instance
+		'''
+		self._client = client
+		self._ocr = ocr
+		self.state = enum.State.INIT
+		self.image = ""
+		self.extracted_string = ""
+		self.error = 'None'
+
+	def handler_done_long_polling(self, image):
+		''' Handler that gets called by the client whenever long-polling has a
+		result that needs servicing.
+		'''
+		self.image = image
+		# Save the image to disk 
+		with open('photo/latest.jpeg', 'wb') as fh:
+			fh.write(self.image)
+		fh.close()
+		print "Received an image from server. Saved it to disk."
+
+	def handler_extracted_text(self, text):
+		''' Handler that gets called by the OCR whenever it has finished 
+		extracting text from a string.
+		'''
+		self.extracted_string = text
+		print "Extracted text: %s" % self.extracted_string.encode('utf-8')
+
+	def set_client_and_ocr(self, client, ocr):
+		''' Set the internal variables _client and _ocr, after they have been
+		initialised with a reference to self
+		'''
+		self._client = client
+		self._ocr = ocr
+
+	def perform_init_sequence(self):
+		''' Initialise the sub-modules of the laptop.
+		'''
+		start_time = time.time()
+		while (time.time() - start_time < 5.0):
+			pass
+
+	def process_state(self):
+		''' Step through the states as needed, queuing tasks in the process.
+		'''
+		if self.state == enum.State.INIT:
+			self.perform_init_sequence()
+			self.state = enum.State.REQUEST_IMAGE
+			return
+
+		elif self.state == enum.State.REQUEST_IMAGE:
+			if self.error == 'None':
+				self._client.post_success()
+			else:
+				self._client.post_failure(self, self.error)
+			self.state = enum.State.INITIATE_LONG_POLL
+			return
+
+		elif self.state == enum.State.INITIATE_LONG_POLL:
+			self._client.queue_long_poll()
+			self.state = enum.State.WAIT_FOR_IMAGE
+			return
+
+		elif self.state == enum.State.WAIT_FOR_IMAGE:
+			# While we're waiting, for the long-poll to finish, do nothing
+			if self._client.is_long_polling:
+				pass
+			# Long polling has finished
+			else:
+				self.state = enum.State.INITIATE_OCR
+			return
+
+		elif self.state == enum.State.INITIATE_OCR:
+			self._ocr.queue_extract_text('photo/latest.jpeg')
+			self.state = enum.State.WAIT_FOR_OCR
+			return
+
+		elif self.state == enum.State.WAIT_FOR_OCR:
+			# Do nothing while we're waiting for OCR to finish
+			if self._ocr.is_extracting:
+				pass
+			# OCR has finished
+			else:
+				self.state = enum.State.PARSE_GESTURES
+			return
+
+		elif self.state == enum.State.PARSE_GESTURES:
+			# TODO: actual gesture parsing
+			# Go back to requesting an image
+			self.state = enum.State.REQUEST_IMAGE
+			return
+
 
 # FUNCTIONS
-def init():
-	''' Initialise the sub-modules of the laptop.
-	'''
-	start_time = time.time()
-	while (time.time() - start_time < 5.0):
-		pass
-
 def main(host, port=80):
 	''' Performs the operations of the laptop client, connecting to a 
 	server on host:port.
 	'''
-	client = LaptopClient(host, port)
 
-	init()
-	print "Processing done: %s" % client.post_success()
+	state_machine = GlobalStateMachine()
+	client = LaptopClient(host, port=port, state_machine=state_machine)
+	ocr = OCRController(state_machine=state_machine)
 
-	picture = client.long_poll()
-	print "Picture: %s received from the server." % picture
+	state_machine.set_client_and_ocr(client, ocr)
+
+	while True:
+		state_machine.process_state()
+
+	# init()
+	# print "Processing done: %s" % client.post_success()
+
+	# picture = client.long_poll()
+	# print "Picture: %s received from the server." % picture
+	# with open('photo/latest.jpeg', 'wb') as fh:
+	# 	fh.write(picture.decode('base64'))
 
 def parse_host_and_port(args=None):
 	''' Returns a tuple of the parsed address and port arguments from the 
